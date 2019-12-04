6d20b318dcb7753f2c3a01f39912847bfa4e61a9
Thomas Beirne <thomas@buildingconnected.com>
2019-12-03 20:28:23 -0800

    Actually solve day 3, part 1

--- 3/part-1.js
+++ 3/part-1.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const path = require('path')
 const { readCommaLists } = require('../h')
 
@@ -5,53 +6,56 @@ const [line1, line2] = readCommaLists(path.join(__dirname, './input.txt'), {
 	parseNumbers: false,
 })
 
-const parseInstruction = instruction => {
-	return [instruction[0], parseInt(instruction.slice(1), 10)]
+const parseInstruction = instruction => [
+	instruction[0],
+	parseInt(instruction.slice(1), 10),
+]
+const performOp = (op, coord) => {
+	switch (op) {
+		case 'U':
+			return { x: coord.x, y: coord.y + 1 }
+		case 'D':
+			return { x: coord.x, y: coord.y - 1 }
+		case 'L':
+			return { x: coord.x - 1, y: coord.y }
+		case 'R':
+			return { x: coord.x + 1, y: coord.y }
+		default:
+			throw new Error('Unknown op', op)
+	}
 }
-function convertToLineSegments(
+function convertToPoints(
 	[instruction, ...line],
-	{ x, y } = { x: 0, y: 0 },
-	segments = []
+	prevCoord = { x: 0, y: 0 },
+	points = []
 ) {
 	const [op, magnitude] = parseInstruction(instruction)
-	let nextCoord
-	if (op === 'U') {
-		nextCoord = { x, y: y + magnitude }
-	} else if (op === 'D') {
-		nextCoord = { x, y: y - magnitude }
-	} else if (op === 'L') {
-		nextCoord = { x: x - magnitude, y }
-	} else if (op === 'R') {
-		nextCoord = { x: x + magnitude, y }
-	} else {
-		throw new Error('Unknown op', op)
+
+	let curCoord = prevCoord
+	for (let i = 0; i < magnitude; i++) {
+		curCoord = performOp(op, curCoord)
+		points.push(curCoord)
 	}
 
 	if (!line.length) {
-		return segments
+		return points
 	}
-	return convertToLineSegments(line, nextCoord, [
-		...segments,
-		[{ x, y }, nextCoord],
-	])
+	return convertToPoints(line, curCoord, points)
 }
 
-const segments1 = convertToLineSegments(line1)
-const segments2 = convertToLineSegments(line2)
+const points1 = convertToPoints(line1)
+const points2 = convertToPoints(line2)
 
-function getIntersection(seg1, seg2) {
-	// Hrm… how to handle overlapping segments?
+function getIntersections(points1, points2) {
+	return _.intersectionBy(points1, points2, ({ x, y }) => `${x},${y}`)
 }
-function getIntersections(segments1, segments2) {
-	const intersections = []
-	for (let seg1 of segments1) {
-		for (let seg2 of segments2) {
-			const intersection = getIntersection(seg1, seg2)
-			if (intersections) {
-				intersections.push(intersection)
-			}
-		}
-	}
-	return intersections
-}
-getIntersections(segments1, segments2)
+const manhattan = ({ x, y }) => Math.abs(x) + Math.abs(y)
+const closest = getIntersections(points1, points2).reduce(
+	(closest, curPoint) => {
+		return !closest || manhattan(curPoint) < manhattan(closest)
+			? curPoint
+			: closest
+	},
+	undefined
+)
+console.log('Manhattan', manhattan(closest))
